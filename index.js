7ec17e03b3059c7d7e567e6e469e16696a265262
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2017-10-16 20:32:42 +0530

    added tail -f

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
+var Tail = require('always-tail');
+var fs = require('fs');
+var filename = "../processing-engine/nlu.log";
+
+
 io.on('connection', function (socket) {
     console.log('a user connected');
 });
-// enables POST, PUT Body data to be retrieved as JSON
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-app.post('/broadcast', (req, res) => {
-    console.log(req.body);
-    io.emit(req.body.uuid, req.body.messege);
-    res.status(200).json({ status: req.body });
+if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
 
+var tail = new Tail(filename, '\n');
+
+tail.on('line', function(data) {
+ console.log("got line:", data);
+ io.emit("VALUE", data);
 });
+
+tail.on('error', function(data) {
+ console.log("error:", data);
+});
+
+tail.watch();
+
+
+
+
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
