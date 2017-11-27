8f68d56f2fb9b7e2c10e93246f2d3e1f7d737ccd
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2017-11-27 15:57:41 +0530

    added socket

--- index.js
+++ index.js
@@ -15,19 +15,36 @@ if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
 
 var tail = new Tail(filename, '\n');
 
-tail.on('line', function(data) {
- console.log("got line:", data);
- io.emit("VALUE", data);
+tail.on('line', function (data) {
+    console.log("got line:", data);
+    io.emit("VALUE", data);
 });
 
-tail.on('error', function(data) {
- console.log("error:", data);
+tail.on('error', function (data) {
+    console.log("error:", data);
 });
 
 tail.watch();
 
+// ############# OVERALL ################### //
+var overallname = "../overall.log";
 
 
+if (!fs.existsSync(overallname)) fs.writeFileSync(overallname, "");
+
+var overallTail = new Tail(overallname, '\n');
+
+overallTail.on('line', function (data) {
+    console.log("emitting", data);
+    io.emit("OVERALL", data);
+});
+
+overallTail.on('error', function (data) {
+    console.log("error:", data);
+});
+
+overallTail.watch();
+// ############# OVERALL END ################### //
 
 
 http.listen(3000, function () {
